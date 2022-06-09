1dd7ada4c692b74c1e937257a139ebdb03ac08ae
huanchen <huanchen@umich.edu>
2022-06-09 01:56:29 -0400

    all unit tests

--- UNIT_TESTS.py
+++ UNIT_TESTS.py
@@ -3,7 +3,7 @@ import os
 path = os.path.abspath(os.getcwd())
 path = os.path.join(path, "testcases/UNIT_TESTS")
 unit_tests = []
-for i in range(24, 25):
+for i in range(29):
     unit_tests.append(os.path.join(path, f"test{i}/test.py"))
 
 for unit_test in unit_tests:
