aa4d2342d9e49366b3cfa4088639b0f4f9f209b6
huanchen <huanchen@umich.edu>
2022-06-08 23:02:18 -0400

    20 unit tests

---
+++ UNIT_TESTS.py
@@ -0,0 +1,10 @@
+import os
+
+path = os.path.abspath(os.getcwd())
+path = os.path.join(path, "testcases/UNIT_TESTS")
+unit_tests = []
+for i in range(16, 21):
+    unit_tests.append(os.path.join(path, f"test{i}/test.py"))
+
+for unit_test in unit_tests:
+    exec(open(unit_test).read())
---
+++ testcases/UNIT_TESTS/test6/test.py
@@ -0,0 +1,31 @@
+"""This test focus on sequential execution on a single device."""
+
+import os
+import sys
+
+import simulator
+
+path = os.path.abspath(os.getcwd())
+path = os.path.join(path, "testcases/UNIT_TESTS/test6/")
+dep = os.path.join(path, "dep.csv")
+prof = os.path.join(path, "prof.csv")
+part = os.path.join(path, "part.csv")
+priority = os.path.join(path, "priority.csv")
+
+out = os.path.join(path, "out")
+sys.stdout = open(out, "w")
+
+simulator.Simulator(
+    dep,
+    [
+        prof,
+        prof,
+        prof,
+        prof,
+        prof,
+        prof,
+    ],
+    part_filename=part,
+    # priority_filename=priority,
+    # bandwidth = 200,
+)
