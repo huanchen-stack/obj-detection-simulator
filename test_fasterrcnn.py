b1e62b53e6eee7f2b52727855e6afa7bf187d5b2
huanchen <huanchen@umich.edu>
2022-06-10 01:12:38 -0400

    faster test

--- test_fasterrcnn.py
+++ test_fasterrcnn.py
@@ -7,7 +7,7 @@ path = os.path.join(path, "testcases/fasterrcnn")
 
 dep = os.path.join(path, "dep.csv")
 prof = os.path.join(path, "prof.csv")
-part = os.path.join(path, "part.csv")
+part = os.path.join(path, "part_botec.csv")
 priority = os.path.join(path, "priority.csv")
 
 out = os.path.join(path, "out")
@@ -24,6 +24,6 @@ simulator.Simulator(
         prof,
     ],
     part_filename=part,
-    # priority_filename=priority,
-    bandwidth = 2000,
+    priority_filename=priority,
+    bandwidth = 1500,
 )
