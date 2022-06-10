ff50d04e2e90310389110a927a086225f0612542
huanchen <huanchen@umich.edu>
2022-06-10 18:10:11 -0400

    FASTERRCNN TESTS w/out BOTEC

---
+++ testcases/FASTERRCNN_TESTS/test1/test.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import simulator
+
+path = os.path.abspath(os.getcwd())
+path = os.path.join(path, "testcases/FASTERRCNN_TESTS/test1/")
+dep = os.path.join(path, "dep.csv")
+prof = os.path.join(path, "prof.csv")
+part = os.path.join(path, "part_same.csv")
+priority = os.path.join(path, "priority.csv")
+
+out = os.path.join(path, "out")
+sys.stdout = open(out, "w")
+
+simulator.Simulator(
+    dep,
+    [
+        prof,
+        prof,
+        prof,
+        prof,
+        prof,
+        prof,
+    ],
+    part_filename=part,
+    priority_filename=priority,
+    # bandwidth = 200,
+)
